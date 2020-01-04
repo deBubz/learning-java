b3e8126ee1748a5417273ad1516709505495a6f7
deBubz <bao.es.hoang2@gmail.com>
2020-01-05 09:55:40 +1100

    isitny maven setup

---
+++ proj/IsitNy/src/main/java/bubz/App.java
@@ -0,0 +1,20 @@
+package bubz;
+import java.util.*;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        Date date = new Date();
+
+        System.out.println("Hello World! " + date);
+    }
+}
