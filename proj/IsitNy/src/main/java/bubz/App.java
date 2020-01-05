4738770f71142c4746d01723bff15a8af1c4fc8e
deBubz <bao.es.hoang2@gmail.com>
2020-01-05 13:02:24 +1100

    Took too long for this

--- proj/IsitNy/src/main/java/bubz/App.java
+++ proj/IsitNy/src/main/java/bubz/App.java
@@ -1,4 +1,6 @@
 package bubz;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -14,7 +16,21 @@ public final class App {
      */
     public static void main(String[] args) {
         Date date = new Date();
+        SimpleDateFormat mFormat = new SimpleDateFormat("MM");
+        SimpleDateFormat dFormat = new SimpleDateFormat("DD");
+        SimpleDateFormat yFormat = new SimpleDateFormat("yyyy");
+        int currMonth = Integer.parseInt(mFormat.format(date));
+        int currDay = Integer.parseInt(dFormat.format(date));
+        int currYear = Integer.parseInt(yFormat.format(date));
 
-        System.out.println("Hello World! " + date);
+        boolean mGate, dGate;
+
+        if(currMonth > 1) {
+            System.out.println("Its not NY yet, next year(" + (currYear + 1) + ") is in" + (12 - currMonth) + " months.");
+        }
+        else {
+            System.out.println("Did you say goobye to " + (currYear - 1));
+            System.out.println("Happy " + currYear + ", enjoy it its only " + currDay + " days into the year.");
+        }
     }
 }
