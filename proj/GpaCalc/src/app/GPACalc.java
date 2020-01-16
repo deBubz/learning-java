a5f967895fa4684a3e02a3c7a87a6d6f3ce9c725
bubz <bao.es.hoang2@gmail.com>
2020-01-16 16:52:11 +1100

    update gpa marks

--- proj/GpaCalc/src/app/GPACalc.java
+++ proj/GpaCalc/src/app/GPACalc.java
@@ -5,31 +5,30 @@ package app;
  */
 
 public class GPACalc {
-    Grade[] gradesArr = new Grade[] {
-        new Grade("HD", 4),
-        new Grade("D", 3.5),
-        new Grade("C", 2.5),
-        new Grade("P", 1.5),
-        new Grade("F", 0.5)
+    Grade[] gradesArr = new Grade[] { new Grade("HD", 7), new Grade("D", 6), new Grade("C", 5), new Grade("P", 4),
+            new Grade("F", 0)
+            // new Grade("HD", 4), new Grade("D", 3.5), new Grade("C", 2.5), new Grade("P",
+            // 1.5), new Grade("F", 0.5)
     };
+
     int totalCredit = 0;
     int totalPoint = 0;
- 
-    public void use(String[] args){                     // quick
-        for(int i = 0; i < gradesArr.length; i++) {
+
+    public void use(String[] args) { // quick
+        for (int i = 0; i < gradesArr.length; i++) {
             // add gradess
-            addingGrades(i, Integer.parseInt(args[i+1]));
+            addingGrades(i, Integer.parseInt(args[i + 1]));
         }
         // print input
         for (Grade grade : gradesArr) {
-           print("You entered " + grade.getGrdCount() +" "+ grade.getGrdName() + "(s)\n"); 
+            print("You entered " + grade.getGrdCount() + " " + grade.getGrdName() + "(s)\n");
         }
         printResult();
     }
 
-    public void use() {                                 // manual
+    public void use() { // manual
         println("ENTER WHAT YOU GOT");
-        for(int i = 0; i < gradesArr.length; i++) {
+        for (int i = 0; i < gradesArr.length; i++) {
             print("How many " + gradesArr[i].getGrdName() + " you got: ");
             addingGrades(i, In.nextInt());
         }
@@ -37,7 +36,7 @@ public class GPACalc {
         printResult();
     }
 
-    public void addingGrades(int i, int gradecount) {   // there must be a better way doing this
+    public void addingGrades(int i, int gradecount) { // there must be a better way doing this
         gradesArr[i].setGrdCount(gradecount);
         totalCredit += gradesArr[i].getTotalCredit();
         totalPoint += gradesArr[i].getTotalGrade();
@@ -48,10 +47,15 @@ public class GPACalc {
         println("Assunimg each subj is 6cp");
         println("Credit = " + totalCredit);
         println("Total = " + totalPoint);
-        println("GPA = " + (totalPoint / totalCredit));
+        println("GPA = " + ((float) totalPoint / totalCredit));
         println("******************************************");
     }
 
-    public static void println(String txt) { System.out.println(txt); }
-    public static void print(String txt) { System.out.print(txt); }
+    public static void println(String txt) {
+        System.out.println(txt);
+    }
+
+    public static void print(String txt) {
+        System.out.print(txt);
+    }
 }
