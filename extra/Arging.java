ea33520d0fdda906041ca313279725a606d410b1
deBubz <bao.es.hoang2@gmail.com>
2020-03-04 03:56:43 +1100

    reorganising

--- extra/Arging.java
+++
@@ -1,5 +0,0 @@
-public class Arging {
-	public static void main(String[] args) {
-		System.out.println("Hey " + args[0]);
-	}
-}
---
+++ proj/extra/Arging.java
@@ -0,0 +1,8 @@
+// testing how arguements works
+
+public class Arging {
+	public static void main(String[] args) {
+		// System.out.println("Hey " + args[0]);
+		System.out.println("hey");
+	}
+}
